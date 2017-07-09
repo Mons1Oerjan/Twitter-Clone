f2d4f29526000e77edbd770d50ae40f66e8acc36
Orjan Monsen <mons@dal.ca>
2017-07-09 20:30:05 -0300

    Finished user signup

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -2,9 +2,13 @@ require 'test_helper'
 
 class ApplicationHelperTest < ActionView::TestCase
 
-  test "full title helper" do
-    assert_equal full_title, "Ruby on Rails Tutorial Sample App"
-    assert_equal full_title("Help"), "Help | Ruby on Rails Tutorial Sample App"
+  def setup
+    @full_title = 'Ruby on Rails Tutorial Sample App'
+  end
+
+  test 'full title helper' do
+    assert_equal full_title, "#{@full_title}"
+    assert_equal full_title('Help'), "Help | #{@full_title}"
   end
 
 end
